cd6dbfd1bf154cbb16f83bdf291dfdd06114ea9c
Kyle Thompson <me@kkt.dev>
2025-12-04 21:49:39 -0800

    Initial structure and copy

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kyle Thompson";
-export const SITE_DESCRIPTION = "Kyle Thompson's personal blog";
+export const SITE_DESCRIPTION =
+  "Building better CI at RWX. Private pilot. Dog person.";
